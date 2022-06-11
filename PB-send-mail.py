bdb7aaae279a4ea1137a85f860d289e11916a96e
devfranklindiaz <dev.franklin.diaz@gmail.com>
2022-06-11 03:56:13 -0300

    Agregado de comment

--- PB-send-mail.py
+++ PB-send-mail.py
@@ -5,7 +5,7 @@ message = 'Subject: {}\n\n{}'.format('Subject', 'DESCRIPTIONS')
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 email= 'YOUR EMAIL'
-server.login(email, os.getenv("CLV")) # CLV as environment variable if you would like to do so
+server.login(email, os.getenv("CLV")) # CLV as environment variable if you would like to do so. You need the app password of gmail, you can generate in your gmail account manager
 
 server.sendmail(email, 'email-receptor', message)
 
